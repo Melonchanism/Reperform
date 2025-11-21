89661347ed202c2453bd9cddc6429f2f511fe19d
Melonchanism <121840186+Melonchanism@users.noreply.github.com>
2025-11-21 00:34:27 -0800

    fix: more strict error checking
    Signed-off-by: Melonchanism <121840186+Melonchanism@users.noreply.github.com>

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -2,20 +2,20 @@ import { supabase } from "$lib/supabase";
 import type { PageLoad } from "./$types";
 
 export const load: PageLoad = async () => {
-  const { data, error } = await supabase.from("recordings").select();
+	const { data, error } = await supabase.from("recordings").select();
 
-  console.log(error);
+	console.log(error);
 
-  if (error) throw error;
+	if (error !== null) throw error;
 
-  let sorted = new Map<string, RecordItem[]>();
+	let sorted = new Map<string, RecordItem[]>();
 
-  for (const item of data as RecordItem[]) {
-    if (sorted.get(item.date) == null) {
-      sorted.set(item.date, [item]);
-    } else {
-      sorted.get(item.date)?.push(item);
-    }
-  }
-  return { sorted };
+	for (const item of data as RecordItem[]) {
+		if (sorted.get(item.date) == null) {
+			sorted.set(item.date, [item]);
+		} else {
+			sorted.get(item.date)?.push(item);
+		}
+	}
+	return { sorted };
 };
